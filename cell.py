85d0f278fba5cb53502aa904e1c530ee1419ef8e
pdashmilly <paul.mcmillian.pro@gmail.com>
2026-04-26 23:35:17 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,8 +1,9 @@
+"""Simple Cell class for beginner version."""
 
 
 class Cell:
-    # This class stores one position in the map
     def __init__(self, row, col, walkable=True, symbol='.'):
+        # Store core cell metadata used throughout the pathfinding workflow.
         self.row = row
         self.col = col
         self.walkable = walkable
@@ -10,23 +11,27 @@ class Cell:
         self.parent = None
 
     def __hash__(self):
-        # Hash by row/col so this object can be used in sets and dicts
+        # Use coordinates as stable identity so Cell works in sets and dictionaries.
         return hash((self.row, self.col))
 
     def __eq__(self, other):
-        # Two cells are equal if they have the same coordinates
+        # Two cells are equal when they represent the same grid position.
         if not isinstance(other, Cell):
             return False
         return self.row == other.row and self.col == other.col
 
     def is_walkable(self):
+        # Expose walkability through a small helper method.
         return self.walkable
 
     def get_position(self):
+        # Return coordinates as a tuple for display/debugging convenience.
         return (self.row, self.col)
 
     def set_parent(self, cell):
+        # Optionally store a back-reference to the previous cell.
         self.parent = cell
 
     def get_parent(self):
+        # Return previously stored parent cell (or None).
         return self.parent
